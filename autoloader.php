abb8561a3aabf688ec22b35672df33e036dcab9f
Emerson Souza <ebsouzapro@gmail.com>
2017-06-02 22:53:51 -0300

    Start

---
+++ autoloader.php
@@ -0,0 +1,12 @@
+<?php
+
+
+	/*
+
+	Este arquivo contém o código responsável por importar(ler sobre spl_autoload) as classes necessárias durante a execução do sistema
+
+	*/
+
+	//Importar e instanciar a "main" do sistema
+	//Iniciar execução do sistema
+?>
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+	/**
+
+	Aqui ficarão as configurações gerais do sistema, tais como: Login e senha do banco de dados, Ambiente de desenvolvimento, etc...
+
+	**/
+	require_once("autoloader.php");
+
+?>
