d44d67bfce4cd18cf9dc274ea7d0e92c14bd3b08
vvalmeida <vvalmeida96@gmail.com>
2017-06-06 21:06:13 -0300

    Documentação da classe usuário.

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Classe responsável por representar um usuário no contexto do sistema.
+ * @author 
+ *
+ */
 class Usuario {
 
     //Adicionei um método para gerarId
@@ -13,7 +18,14 @@ class Usuario {
     private $estaAtivo;
     private static $contId = 0;
 
-    //construtor da classe
+    /**
+     * Construtor da classe
+     * @param unknown $email email do usuário
+     * @param unknown $nome nome do usuário
+     * @param unknown $sobrenome sobrenome do usuário
+     * @param unknown $senha senha do usuário
+     * @param unknown $estaAtivo indica se está ativo
+     */
     public function __construct($email, $nome, $sobrenome, $senha, $estaAtivo) {
         $this->geraId();
         $this->email = $email;
@@ -23,52 +35,98 @@ class Usuario {
         $this->estaAtivo = $estaAtivo;
     }
 
+    /**
+     * Gera automaticamente id do usuário.
+     */
     private function gerarId() { //gera o id do usuário
         $this->id = $contId;
         Usuario::$contId++;
     }
 
-    /*Getters e setters necessários*/
+    /**
+     * Obtém o id do usuário.
+     * @return id
+     */
     public function getId() {
         return $id;
     }
 
+    /**
+     * Obtém o email do usuário.
+     * @return email
+     */
     public function getEmail() {
         return $email;
     }
 
+    /**
+     * Configura o email do usuário.
+     * @param unknown $email email do usuário
+     */
     public function setEmail($email) {
         $this->email = $email;
     }
 
+    /**
+     * Obtém o nome do usuário.
+     * @return nome
+     */
     public function getNome() {
         return $nome;
     }
 
+    /**
+     * Configura o nome do usuário.
+     * @param unknown $nome nome do usuário
+     */
     public function setNome($nome) {
         $this->nome = $nome;
     }
 
+    /**
+     * Obtém o sobrenome do usuário.
+     * @return sobrenome
+     */
     public function getSobrenome() {
         return $sobrenome;
     }
 
+    /**
+     * Configura o sobrenome do usuário.
+     * @param unknown $sobrenome sobrenome do usuário
+     */
     public function setSobrenome($sobrenome) {
         $this->sobrenome = $sobrenome;
     }
 
+    /**
+     * Obtém a senha do usuário.
+     * @return senha
+     */
     public function getSenha() {
         return $senha;
     }
 
+    /**
+     * Configura a senha do usuário.
+     * @param unknown $senha senha do usuário
+     */
     public function setSenha($senha) {
         $this->senha = $senha;
     }
 
+    /**
+     * Verifica se o usuário está ativo.
+     * @return <code>true</code>, se ele está ativo; <code>false</code>, caso contrário
+     */
     public function isAtivo() {
         return $estaAtivo;
     }
 
+    /**
+     * Configura se o usuário está ativo.
+     * @param unknown $estaAtivo indicação se o usuário está ativo
+     */
     public function setEstaAtivo($estaAtivo) {
         $this->estaAtivo = $estaAtivo;
     }
