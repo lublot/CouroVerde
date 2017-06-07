f400739cd37e0aa87d5c47aa6e79f50be362c608
vvalmeida <vvalmeida96@gmail.com>
2017-06-07 15:56:36 -0300

    Correções - Classe funcionário completa e documentada

--- models/Funcionario.php
+++ models/Funcionario.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Classe responsável por modelar um funcionário no contexto do sistema
- * @author MItologhic Software
- *
- */
+* Classe responsável por modelar um funcionário no contexto do sistema
+* @author MItologhic Software
+*
+*/
 class Funcionario extends Usuario {
 	
 	private $matricula;
@@ -18,167 +18,174 @@ class Funcionario extends Usuario {
 	private $podeRealizarBackup;
 	
 	/**
-	 * Construtor da classe
-	 * @param unknown $email email do usuário
-	 * @param unknown $nome nome do usuário
-	 * @param unknown $sobrenome sobrenome do usuário
-	 * @param unknown $senha senha do usuário
-	 * @param unknown $matricula matrícula do funcionário
-	 * @param unknown $funcao função do funcionário no museu
-	 * @param unknown $podeCadastrarObra indica se pode cadastrar obra
-	 * @param unknown $podeGerenciarObra indica se pode gerenciar obra
-	 * @param unknown $podeRemoverObra indica se pode remover obra
-	 * @param unknown $podeCadastrarNoticia indica se pode cadastrar notícia
-	 * @param unknown $podeGerenciarNoticia indica se pode gerenciar notícia
-	 * @param unknown $podeRealizarBackup indica se pode realizar backup
-	 * 
-	 */
-	public function __construct($id,$email, $nome, $sobrenome, $senha, $confirmouCadastro, $matricula, $funcao, $podeCadastrarObra,$podeGerenciarObra, $podeGerenciarObra, $podeRemoverObra, $podeCadastrarNoticia, $podeGerenciarNoticia, $podeRemoverNoticia, $podeRealizarBackup) {
+	* Construtor da classe
+	* @param unknown $email email do usuário
+	* @param unknown $nome nome do usuário
+	* @param unknown $sobrenome sobrenome do usuário
+	* @param unknown $senha senha do usuário
+	* @param unknown $matricula matrícula do funcionário
+	* @param unknown $funcao função do funcionário no museu
+	* @param unknown $podeCadastrarObra indica se pode cadastrar obra
+	* @param unknown $podeGerenciarObra indica se pode gerenciar obra
+	* @param unknown $podeRemoverObra indica se pode remover obra
+	* @param unknown $podeCadastrarNoticia indica se pode cadastrar notícia
+	* @param unknown $podeGerenciarNoticia indica se pode gerenciar notícia
+	* @param unknown $podeRealizarBackup indica se pode realizar backup
+	* 
+	*/
+	public function __construct($id,$email, $nome, $sobrenome, $senha, $confirmouCadastro, $matricula, $funcao, $podeCadastrarObra,$podeGerenciarObra, $podeRemoverObra, $podeCadastrarNoticia, $podeGerenciarNoticia, $podeRemoverNoticia, $podeRealizarBackup) {
 		parent::__construct($id, $email, $nome, $sobrenome, $senha, $confirmouCadastro);
-		this->matricula = $matricula;
-		
+		$this->matricula = $matricula;
+		$this->funcao = $funcao;
+		$this->podeCadastrarObra = $podeCadastrarObra;
+		$this->podeGerenciarObra = $podeGerenciarObra;
+		$this->podeRemoverObra = $podeRemoverObra;
+		$this->podeCadastrarNoticia = $podeCadastrarNoticia;
+		$this->podeGerenciarNoticia = $podeGerenciarNoticia;
+		$this->podeRealizarBackup = $podeRealizarBackup;
+
 	}
 	
 	/**
-	 * Obtém matrícula do funcionário.
-	 * @return matrícula do funcionário
-	 */
+	* Obtém matrícula do funcionário.
+	* @return matricula
+	*/
 	public function getMatricula() {
 		return $this->matricula;
 	}
 	
 	/**
-	 * Configura a matrícula do funcionário.
-	 * @param unknown $matricula matrícula do funcionário
-	 */
+	* Configura a matrícula do funcionário.
+	* @param unknown $matricula matrícula do funcionário
+	*/
 	public function setMatricula($matricula) {
 		$this->matricula = matricula;
 	}
 	
 	/**
-	 * Obtém a função do funcionário no museu.
-	 * @return função do funcionário
-	 */
+	* Obtém a função do funcionário no museu.
+	* @return funcao
+	*/
 	public function getFuncao() {
 		return $this->funcao;
 	}
 	
 	/**
-	 * Configura a função do funcionário.
-	 * @param unknown $funcao função do funcionário
-	 */
+	* Configura a função do funcionário.
+	* @param unknown $funcao função do funcionário
+	*/
 	public function setFuncao($funcao) {
 		$this->funcao = $funcao;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode cadastrar obras.
-	 * @return indicação se o funcionário pode cadastrar obras.
-	 */
+	* Verifica se o funcionário pode cadastrar obras.
+	* @return <code>true</code>, caso possa cadadastrar obras; <code>false</code>, caso contrário.
+	*/
 	public function isPodeCadastrarObra() {
 		return $this->podeCadastrarObra;
 	}
 	
 	/**
-	 * Configura se o funcionário pode cadastrar obras.
-	 * @param unknown $podeCadastrarObra indicação se o funcionário pode cadastrar obras
-	 */
+	* Configura se o funcionário pode cadastrar obras.
+	* @param unknown $podeCadastrarObra indicação se o funcionário pode cadastrar obras
+	*/
 	public function setPodeCadastrarObra($podeCadastrarObra) {
 		$this->podeCadastrarObra = $podeCadastrarObra;
 	}
 
 	/**
-	 * Verifica se o funcionário pode gerenciar obras.
-	 * @return indicação se o funcionário pode gerenciar obras.
-	 */
+	* Verifica se o funcionário pode gerenciar obras.
+	* @return <code>true</code>, caso possa gerenciar obras; <code>false</code>, caso contrário.
+	*/
 	public function isPodeGerenciarObra() {
 		return $this->podeGerenciarObra;
 	}
 	
 	/**
-	 * Configura se o funcionário pode gerenciar obras.
-	 * @param unknown $podeGerenciarObra indicação se o usuário pode gerenciar obras
-	 */
+	* Configura se o funcionário pode gerenciar obras.
+	* @param unknown $podeGerenciarObra indicação se o usuário pode gerenciar obras
+	*/
 	public function setPodeGerenciarObra($podeGerenciarObra) {
 		$this->podeGerenciarObra = $podeGerenciarObra;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode remover obras.
-	 * @return indicação se o funcionário pode remover obras.
-	 */
+	* Verifica se o funcionário pode remover obras.
+	* @return <code>true</code>, caso possa remover obras; <code>false</code>, caso contrário.
+	*/
 	public function isPodeRemoverObra() {
 		return $this->podeRemoverObra;
 	}
 	
 	/**
-	 * Configura se o usuário pode remover obras.
-	 * @param unknown $podeRemoverObra indicação se o funcionário pode remover obras
-	 */
+	* Configura se o usuário pode remover obras.
+	* @param unknown $podeRemoverObra indicação se o funcionário pode remover obras
+	*/
 	public function setPodeRemoverObra($podeRemoverObra) {
 		$this->podeRemoverObra = $podeRemoverObra;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode cadastrar notícias.
-	 * @return indicação se o funcionário pode cadastrar notícias.
-	 */
+	* Verifica se o funcionário pode cadastrar notícias.
+	* @return <code>true</code>, caso possa cadastrar notícias; <code>false</code>, caso contrário.
+	*/
 	public function isPodeCadastrarNoticia() {
 		return $this->podeCadastrarNoticia;
 	}
 	
 	/**
-	 * Configura se o funcionário pode cadastrar notícias.
-	 * @param unknown $podeCadastrarNotícia indica se o funcionário pode cadastrar notícia.
-	 */
+	* Configura se o funcionário pode cadastrar notícias.
+	* @param unknown $podeCadastrarNotícia indica se o funcionário pode cadastrar notícia.
+	*/
 	public function setPodeCadastrarNoticia($podeCadastrarNoticia) {
 		$this->podeCadastrarNoticia = $podeCadastrarNoticia;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode gerenciar notícias.
-	 * @return indicação se o funcionário pode gerenciar notícias
-	 */
+	* Verifica se o funcionário pode gerenciar notícias.
+	* @return <code>true</code>, caso possa gerenciar notícias; <code>false</code>, caso contrário.
+	*/
 	public function isPodeGerenciarNoticia() {
 		return $this->podeGerenciarNoticia;
 	}
 	
 	/**
-	 * Configura se o funcionário pode gerenciar notícias.
-	 * @param unknown $podeGerenciarNoticia indicação se o funcionário pode gerenciar notícias.
-	 */
+	* Configura se o funcionário pode gerenciar notícias.
+	* @param unknown $podeGerenciarNoticia indicação se o funcionário pode gerenciar notícias.
+	*/
 	public function setPodeGerenciarNoticia($podeGerenciarNoticia) {
 		$this->podeGerenciarNoticia = $podeGerenciarNoticia;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode remover notícias.
-	 * @return indicação se o funcionário pode remover notícias.
-	 */
+	* Verifica se o funcionário pode remover notícias.
+	* @return <code>true</code>, caso possa remover notícias; <code>false</code>, caso contrário.
+	*/
 	public function isPodeRemoverNoticia() {
 		return $this->podeRemoverNoticia;
 	}
 	
 	/**
-	 * Configura se o funcionário pode remover notícias.
-	 * @param unknown $podeRemoverNoticia indicação se o funcionário pode remover notícias.
-	 */
+	* Configura se o funcionário pode remover notícias.
+	* @param unknown $podeRemoverNoticia indicação se o funcionário pode remover notícias.
+	*/
 	public function setPodeRemoverNoticia($podeRemoverNoticia) {
 		$this->podeRemoverNoticia = $podeRemoverNoticia;
 	}
 	
 	/**
-	 * Verifica se o funcionário pode realizar backup.
-	 * @return indicação se o funcionário pode realizar backup.
-	 */
+	* Verifica se o funcionário pode realizar backup.
+	* @return <code>true</code>, caso possa realizar backups; <code>false</code>, caso contrário.
+	*/
 	public function isPodeRealizarBackup() {
 		return $this->podeRealizarBackup;
 	}
 	
 	/**
-	 * Configura se o funcionário pode  realizar backups.
-	 * @param unknown $podeRealizarBackup indicação se o funcionário pode realizar backup.
-	 */
+	* Configura se o funcionário pode  realizar backups.
+	* @param unknown $podeRealizarBackup indicação se o funcionário pode realizar backup.
+	*/
 	public function setPodeRealizarBackup($podeRealizarBackup) {
 		$this->podeRealizarBackup = $podeRealizarBackup;
 	}
