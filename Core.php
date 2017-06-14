661582b0b60a1d95762c0f2b38f759bc942edce8
vvalmeida <vvalmeida96@gmail.com>
2017-06-14 20:18:31 -0300

    Renomei controller de login pra ficar de acordo com diagrama de classes.

--- Core.php
+++ Core.php
@@ -9,7 +9,7 @@ class Core{
 
     public function run() {
  
-        $path = explode("index.php",$_SERVER['PHP_SELF']);// Divide a string da url
+        $path = explode($ds,$_SERVER['PHP_SELF']);// Divide a string da url
         $path = end($path);//Pega o final do array
         
         if(!empty($path)){//Verifica se contém algo
