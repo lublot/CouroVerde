acf79ade58e36065c5f44062d41623ff5b3920d2
vvalmeida <vvalmeida96@gmail.com>
2017-06-26 15:19:59 -0300

    PSR-4 completo

--- autoloader.php
+++ autoloader.php
@@ -1,5 +1,6 @@
 <?php
-require_once dirname(__DIR__).'\vendor\autoload.php';
+require_once __DIR__.'\vendor\autoload.php';
+require_once __DIR__.'\Core.php';
 use \exceptions\ArquivoNaoEncontradoException as ArquivoNaoEncontradoException;
 
 	/*
