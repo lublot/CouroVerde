6258965b7629fb98b0a8b49eb7d01271ebabb58b
root <root@unix>
2017-06-27 22:37:28 -0300

    correção do caminho do teste do controller

--- test/controllersTest/cadastroControllerTest.php
+++ test/controllersTest/cadastroControllerTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(dirname(__DIR__)).'\vendor\autoload.php';
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
 
 //define(ABSPATH,dirname(__DIR__).'\..');
 use \controllers\cadastroController as cadastroController;
