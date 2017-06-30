941a12069c5c32ad15509ed272b50f41cd8e3393
EmersonSouzaBr <ebsouzapro@gmail.com>
2017-06-30 19:56:53 -0300

    Corrigido bug google

--- vendor/credentialsConfig.php
+++ vendor/credentialsConfig.php
@@ -3,7 +3,7 @@
     //Script de Configuração de Credenciais
     require_once ABSPATH.'/vendor/autoload.php';
 
-    $scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI'];
+    $scriptUri = ROOT_URL.'login/acessoGoogle';
     $client = new \Google_Client();
     $client->setAccessType('online'); // default: offline
     $client->setApplicationName('WebMuseu - Casa do Sertão');
