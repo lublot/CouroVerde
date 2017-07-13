c5236e450fe58e8b65a6462f9e6ddb4a5ac130c3
brunomlima11 <brunomenezes253@gmail.com>
2017-07-13 17:56:44 -0300

    atualização de progrsso de cadastro de obras

---
+++ views/assets/js/cadObras-script.js
@@ -0,0 +1,48 @@
+// Inteiro para armazenar a página atual do usuário
+var pagAtual = 1;
+// Inteiro para armazenar o número limite de páginas disponíveis
+var pagMax = 5;
+
+// Função responsável para passar para a próxima página
+function avancarPag() {
+    // Verifica se a página atual do usuário excedeu o número limite máximo de páginas
+    if (pagAtual < pagMax) {
+        // String para concatenar o ID da página do HTML com a variável que armazena a página atual
+        var pageOld = "#page_" + pagAtual;
+        // Atualiza a visualização da tela atual para "nenhum"
+        $(pageOld).css("display", "none");
+
+        // Incrementa a variável para avançar a página
+        pagAtual++;
+        // String para concatenar o ID da página do HTML com a variável que armazena a próxima página
+        var pageNew = "#page_" + pagAtual;
+        // Atualiza a visualização da próxima tela para exibir com um Fade In
+        $(pageNew).fadeIn(750);
+    }
+}
+
+function voltarPag() {
+    // Verifica se a página atual do usuário excedeu o número limite mínimo de páginas
+    if (pagAtual > 1) {
+        // String para concatenar o ID da página do HTML com a variável que armazena a página atual
+        var pageOld = "#page_" + pagAtual;
+        // Atualiza a visualização da tela atual para "nenhum"
+        $(pageOld).css("display", "none");
+
+        // Diminui a variável para retornar a página
+        pagAtual--;
+        // String para concatenar o ID da página do HTML com a variável que armazena a página anterior
+        var pageNew = "#page_" + pagAtual;
+        // Atualiza a visualização da tela anterior para exibir com um Fade In
+        $(pageNew).fadeIn(750);
+    }
+}
+
+// function atualizarTextoBotao() {
+//     if(pagAtual = pagMax) {
+//         $("#btn-confirmar").value="Confirmar";
+//     }
+//     if(pagAtual = 1) {
+//         $("btn-cancelar").value="Cancelar";
+//     }
+// }
