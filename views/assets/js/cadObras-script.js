b385e07281970d23bfa66c0f25ad2255e283d06f
brunomlima11 <brunomenezes253@gmail.com>
2017-07-13 19:00:59 -0300

    comentários e limpeza de código

--- views/assets/js/cadObras-script.js
+++ views/assets/js/cadObras-script.js
@@ -3,24 +3,32 @@ var pagAtual = 1;
 // Inteiro para armazenar o número limite de páginas disponíveis
 var pagMax = 5;
 
+// Função responsável por atualizar a mensagem contida nos botões "Próximo" e "Retroceder" para "Confirmar" e "Cancelar" na ocasião adequada
 function atualizarTextoBotao() {
+    // Caso o usuário esteja na última página do cadastro
     if(pagAtual == pagMax) {
+        //Atualiza o texto do botão direito para "Confirmar"
         $("#btn-confirmar").html('Confirmar');
     } 
+    //Caso contrário
     else {
+        // Deixa o texto "Próximo"
         $("#btn-confirmar").html('Próximo');        
     }
+    //Caso o usuário não esteja na primeira página
     if(pagAtual > 1) {
+        //Atualiza o texto do botão esquerdo para "Retroceder"
         $("#btn-cancelar").html('Retroceder');
     }
+    //Caso contrário
     else {
+        //Deixa o texto "Cancelar"
         $("#btn-cancelar").html('Cancelar');
     }
 }
 
 // Função responsável para passar para a próxima página
 function avancarPag() {
-    console.log(pagAtual);
     // Verifica se a página atual do usuário excedeu o número limite máximo de páginas
     if (pagAtual < pagMax) {
         // String para concatenar o ID da página do HTML com a variável que armazena a página atual
@@ -38,7 +46,6 @@ function avancarPag() {
 }
 
 function voltarPag() {
-    console.log(pagAtual);
     // Verifica se a página atual do usuário excedeu o número limite mínimo de páginas
     if (pagAtual > 1) {
         // String para concatenar o ID da página do HTML com a variável que armazena a página atual
