443a346935abf46cbbfaaa49f352fc45647b3c50
EmersonSouzaBr <ebsouzapro@gmail.com>
2017-07-16 01:34:00 -0300

    Algumas alterações no perfil de usuário

--- controllers/perfilController.php
+++ controllers/perfilController.php
@@ -5,18 +5,20 @@ namespace controllers;
 class perfilController{
     
     public function index(){
-        session_start();
-        $id = $_SESSION['id'];
-        $nome = $_SESSION['nome'];
-        $sobrenome = $_SESSION['sobrenome'];
+        if(isset($_SESSION)){
+            $id = $_SESSION['id'];
+            $nome = $_SESSION['nome'];
+            $sobrenome = $_SESSION['sobrenome'];
 
-        if(isset($_POST) && !empty($_POST)){
-            $this->alterar($_POST);
-        } 
+            if(isset($_POST) && !empty($_POST)){
+                $this->alterar($_POST);
+            }     
+        }
+        
+        
     }
 
     private function alterar($dados){
-        session_start();
         if($this->validarForm($dados)){
             $usuarioDao = new UsuarioDAO();
             $usuarioDao->alterar(array("nome"=>$dados['nome'],"sobrenome"=>$dados['sobrenome']),array("idUsuario"=>$_SESSION['id']));
