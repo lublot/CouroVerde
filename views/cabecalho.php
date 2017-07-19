72eb2ed5e87396d0f4f7419bfeb2d87d0df16648
victormunduruca <victormunduruca@gmail.com>
2017-07-18 21:00:17 -0300

    Logo no cabeçalho adicionado

--- views/cabecalho.php
+++ views/cabecalho.php
@@ -7,7 +7,7 @@
       <div class="row">
           <div class="col-md-1 col-sm-1"></div>
           <div class="col-xs-12 col-md-11">
-              <h2><i class="fa fa-bars"></i> Sertour</h2> 
+                <img src=<?php $this->path('assets/images/header-logo.jpg')?>  class="img-responsive" id="header-logo" style="height: 61; margin-top:10;">
           </div>
 
       </div>
