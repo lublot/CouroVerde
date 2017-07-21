9203ecd0647f3a2ef3c9db46da7c72fe693af94a
aloisiokjr <kleyner2@hotmail.com>
2017-07-21 01:30:27 -0300

    Testes de Cadastro de Obra
    Alguns testes dependem do termino do controller. Falta terminar ainda.

--- test/controllersTest/obraControllerTest.php
+++ test/controllersTest/obraControllerTest.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/../../vendor/autoload.php';
 use \PHPUnit\Framework\TestCase;
 use \DAO\obraDAO as obraDAO;
 use \controllers\obraController as obraController;
-use \exceptions\CampoNoticiaInvalidoException as CampoNoticiaInvalidoException;
+use \exceptions\CampoInvalidoException as CampoInvalidoException;
 use \exceptions\DadosCorrompidosException as DadosCorrompidosException;
 use \models\Obra as Obra;
 
@@ -19,6 +19,66 @@ class obraControllerTest extends TestCase {
         $obraDAO = new obraDAO();
         $obraDAO->remover(array("1"=>"1"));
     }   
+    
+    /**
+    *Testa o cadastro de uma obra com sucesso;
+    * @runInSeparateProcess
+    * @preserveGlobalState disabled 
+    */
+    public function testCadastrarObraSucesso(){
+        $this->instancia->configurarAmbienteParaTeste(0, 'Obra 1','Couro 1', 'Função', 'Origem', 'Procedência', 'Descrição', 0, 0, 'Altura', 'Largura', 'Diâmetro', '10 quilos', '1 metro', 'Materiais', 'Técnicas', 'Autoria', 'Marcas', 'Histórico', 'Modo de Aquisição', 'Data de Aquisição', 'Autor', 'Observações', 'Estado');
+        $this->instancia->cadastrarObra();
+
+        $obraDAO = new obraDAO();
+        $obra = $obraDAO->buscar(array(),array("nome"=> "Obra 1"));
+        $obra->getNome().strcmp('Obra 1');
+        
+
+
+        $numInventario = $obra[0]->getNumInventario();
+        $this->assertEquals(1,count($obra));
+    }
+
+    /**
+    *Testa o cadastro de uma coleção com sucesso;
+    * @runInSeparateProcess
+    * @preserveGlobalState disabled 
+    */
+    public function testCadastrarColecaoSucesso() {
+        //A FAZER
+    }
+
+    /**
+    *Testa o cadastro de uma obra com erro;
+    * @runInSeparateProcess
+    * @preserveGlobalState disabled 
+    */
+    public function testCadastrarObraErro() {
+        $this->instancia->configurarAmbienteParaTeste(0, null,'Couro 1', 'Função', 'Origem', 'Procedência', 'Descrição', 0, 0, 'Altura', 'Largura', 'Diâmetro', '10 quilos', '1 metro', 'Materiais', 'Técnicas', 'Autoria', 'Marcas', 'Histórico', 'Modo de Aquisição', 'Data de Aquisição', 'Autor', 'Observações', 'Estado');
+        $this->expectException(CampoInvalidoException::class); //exceção esperada 
+        $this->instancia->cadastrarObra();
+    }
+
+    /**
+    *Testa o cadastro de uma obra com erro com dados corrompidos;
+    * @runInSeparateProcess
+    * @preserveGlobalState disabled 
+    */
+    public function testCadastrarObraDadosCorrompidos() {
+        $this->instancia->configurarAmbienteParaTeste(0, 'Obra 1','Couro 1', 'Função', 'Origem', 'Procedência', 'Descrição', 0, 0, 'Altura', 'Largura', 'Diâmetro', '10 quilos', '1 metro', 'Materiais', 'Técnicas', 'Autoria', 'Marcas', 'Histórico', 'Modo de Aquisição', 'Data de Aquisição', 'Autor', 'Observações', 'Estado');
+        $_POST = null;
+        $this->expectException(DadosCorrompidosException::class); //exceção esperada 
+        $this->instancia->cadastrarObra();
+    }
+
+    /**
+    *Testa o cadastro de uma coleção com erro;
+    * @runInSeparateProcess
+    * @preserveGlobalState disabled 
+    */
+    public function testCadastrarColecaoErro() {
+        // A FAZER
+    }
 
     /**
     * Testa a edição do nome de uma obra com sucesso;
@@ -38,25 +98,6 @@ class obraControllerTest extends TestCase {
         $this->instancia->configurarAmbienteParaTeste($numInventario ,$novoNome,'Couro 1', 'Função', 'Origem', 'Procedência', 'Descrição', 0, 0, 'Altura', 'Largura', 'Diâmetro', '10 quilos', '1 metro', 'Materiais', 'Técnicas', 'Autoria', 'Marcas', 'Histórico', 'Modo de Aquisição', 'Data de Aquisição', 'Autor', 'Observações', 'Estado');
         $this->instancia->gerenciarObra();
         $obra = $obraDAO->buscar(array(),array("nome"=>$novoNome));
-        $this->assertEquals(1,count($obra));
-    }
-
-    /**
-    *Testa o cadastro de uma obra com sucesso
-    * @runInSeparateProcess
-    * @preserveGlobalState disabled 
-    */
-    public function testCadastrarObraSucesso(){
-        $this->instancia->configurarAmbienteParaTeste(0, 'Obra 1','Couro 1', 'Função', 'Origem', 'Procedência', 'Descrição', 0, 0, 'Altura', 'Largura', 'Diâmetro', '10 quilos', '1 metro', 'Materiais', 'Técnicas', 'Autoria', 'Marcas', 'Histórico', 'Modo de Aquisição', 'Data de Aquisição', 'Autor', 'Observações', 'Estado');
-        $this->instancia->cadastrarObra();
-
-        $obraDAO = new obraDAO();
-        $obra = $obraDAO->buscar(array(),array("nome"=> "Obra 1"));
-        $obra->getNome().strcmp('Obra 1');
-        
-
-
-        $numInventario = $obra[0]->getNumInventario();
         $this->assertEquals(1,count($obra));
     }
 
@@ -101,6 +142,10 @@ class obraControllerTest extends TestCase {
 
         $obra = $obraDAO->buscar(array(),array("nome"=> "Obra Nova"));
         $this->assertEquals(0,count($obra));
+    }
+
+    public function testUploadImagemErro() {
+
     }
 
 }
