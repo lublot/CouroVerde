a94c074e8a94bc9c21108c8c180dfd01c745c278
vvalmeida <vvalmeida96@gmail.com>
2017-08-03 22:08:26 -0300

    pequena mudança no php de upload

--- views/upload.php
+++ views/upload.php
@@ -1,15 +1,19 @@
 <?php
 $uploaded = array();
-
-
+header('Content-Type: application/json');
 if(!empty($_FILES['file']['name'][0])){	
-	// Please implement security measures before deploying to production
-		$pastaImagem = '../media/obras/imagens/'.$_GET["inv"];
-		mkdir($pastaImagem);	
+	$pastaImagem = '../media/obras/imagens/'.$_GET["inv"];
+	mkdir($pastaImagem);	
+	$pasta3D= '../media/obras/modelo3D/'.$_GET["inv"];
+	mkdir($pasta3D);
+		
 	foreach($_FILES['file']['name'] as $position => $name){
-		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], $pastaImagem . '/' . $name)){
+		$ext = explode('.', $name)[1];
+		if(strtoupper($ext) == "OBJ") {
+			move_uploaded_file($_FILES['file']['tmp_name'][$position], $pasta3D . '/' . $name);
+		} else {
+			move_uploaded_file($_FILES['file']['tmp_name'][$position], $pastaImagem . '/' . $name);
 		}
 	}
 }
-
 ?>
