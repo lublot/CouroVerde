42b4e5289b4eef3a8507e03011b659c187ce960a
vvalmeida <vvalmeida96@gmail.com>
2017-08-03 22:51:43 -0300

    mais um errinho no upload arrumado

--- views/upload.php
+++ views/upload.php
@@ -8,6 +8,7 @@ if(!empty($_FILES['file']['name'][0])){
 	mkdir($pasta3D);
 		
 	foreach($_FILES['file']['name'] as $position => $name){
+		$name = mb_convert_encoding($name, "Windows-1252", "UTF-8");
 		$ext = explode('.', $name)[1];
 		if(strtoupper($ext) == "OBJ") {
 			move_uploaded_file($_FILES['file']['tmp_name'][$position], $pasta3D . '/' . $name);
