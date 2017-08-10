2e950191ae07e9e19f52344e2762afbc53502417
IagoAlmeida <iagomcd.almeida@gmail.com>
2017-08-10 00:20:24 -0300

    home semi-final
    se nada mais der certo, pode ser assim msm

--- views/home.php
+++ views/home.php
@@ -9,7 +9,7 @@
     <body>
       <?php $this->carregarCabecalho()?>
       
-      <div class="container-fluid">
+      <div class="container-fluid" style="min-width:100%;">
       
       <div id="carousel-example-generic" class="carousel slide col-xs-12" data-ride="carousel">
         <!-- Indicators -->
@@ -20,28 +20,34 @@
         </ol>
 
         <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-            <img src=<?php $this->path('/assets/images/adm.png')?> alt="..." style="max-height:20%">
-            <div class="carousel-caption">
-                ...
+        <div class="carousel-inner" role="listbox" style="min-width:100%;background:rgba(0,0,0,0)">
+            <div class="item active" style="min-width:100%;">
+                <img  src=<?php $this->path("/assets/images/5.jpeg")?> alt="..." style="min-width:100%;">
+                   
+                <div class="carousel-caption">
+                texto
+                </div>
             </div>
+            <div class="item" style="min-width:100%;">
+                <img  src=<?php $this->path('/assets/images/5.jpeg')?> alt="..." style="min-width:100%;" >
+                <div class="carousel-caption">
+                texto 2
+                </div>
             </div>
-            <div class="item">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
-                ...
+            <div class="item" style="min-width:100%;">
+                <img  src=<?php $this->path('/assets/images/5.jpeg')?> alt="..." style="min-width:100%;" >
+                <div class="carousel-caption">
+                texto 3
+                </div>
             </div>
-            </div>
-            ...
         </div>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" style="background:rgba(0,0,0,0)" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" style="background:rgba(0,0,0,0)"data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
