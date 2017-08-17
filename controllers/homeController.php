8cf0099d47e14bc912a1eec754978257e7f2bd86
EmersonSouzaBr <ebsouzapro@gmail.com>
2017-08-16 22:07:04 -0300

    Home Integrada

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,13 +1,19 @@
 <?php
 namespace controllers;
 
-
+use DAO\NoticiaDAO as NoticiaDAO;
+use models\Noticia as Noticia;
 class homeController extends mainController{
 
-
+    protected $dados = array();
     public function index(){
-        $this->carregarConteudo('home',array());
+        $noticiaDAO = new NoticiaDAO();
+        $noticia = $noticiaDAO->buscarMaisRecente(array(),array(),3);
+        $this->dados['noticias'] = $noticia;
+        $this->carregarConteudo('home',$this->dados);
     }
+
+    
 }
 
 
