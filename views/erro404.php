2df7fa1cd366af5fd963a335261c6891ca022e8d
brunomlima11 <brunomenezes253@gmail.com>
2017-08-17 17:16:23 -0300

    Adição do Link para retornar para a home da Erro404

--- views/erro404.php
+++ views/erro404.php
@@ -8,6 +8,7 @@
 
         <?php $this->carregarCabecalho()?>
         <div class="container">
+            <a href="../index.php"> <h4 class="text-center"> Voltar à pagina inicial </h4></a>
             <img src=<?php $this->path("assets/images/erro404.jpg")?> class="img-responsive center-block">
         </div>
         <?php $this->carregarRodape()?>
