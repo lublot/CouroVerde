78dac326370e8c975d9c47e5ab228813086082c5
EmersonSouzaBr <ebsouzapro@gmail.com>
2017-08-20 19:00:31 -0300

    Links corrigidos

--- views/assets/js/resultadoBusca-script.js
+++ views/assets/js/resultadoBusca-script.js
@@ -89,7 +89,7 @@ function criarBox(dados){
     thumbnail.setAttribute('class','thumbnail');
 
     let imagem = document.createElement('div');
-    imagem.setAttribute('style',"height:80px;background:url("+urlRaiz+dados.caminhoImagem1+");background-size:contain;background-repeat:no-repeat;background-position:center");
+    imagem.setAttribute('style',"height:80px;background:url("+urlRaiz+'/'+dados.caminhoImagem1+");background-size:contain;background-repeat:no-repeat;background-position:center");
     
     thumbnail.appendChild(imagem);
     info.appendChild(titulo);
